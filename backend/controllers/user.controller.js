e5c11c4fabcd99c8b6b4db197cb165bc451a0545
Bence Luo <bence.luo899@gmail.com>
2024-05-06 21:37:10 +0100

    part 3 - Added all functionalities

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,12 +5,11 @@ export const getUserForSidebar = async (req, res) => {
     const loggedInUserId = req.user._id;
     //Every user in our database but not the current authenticated user and not select the password
     const filteredUsers = await User.find({
-      _id: { $ne: loggedInUserId },
-    }).select("-password");
+      _id: { $ne: loggedInUserId } }).select("-password");
 
-    res.status(200).json({ users: filteredUsers });
+    res.status(200).json(filteredUsers);
   } catch (error) {
-    console.error("Error in getUsersForSidebar: ", error.messgae);
+    console.error("Error in getUsersForSidebar: ", error.message);
     res.stauts(500).json({ error: "Internal server error" });
   }
 };
